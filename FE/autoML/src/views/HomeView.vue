3c33aae34f9c312f3988b949f9b1597a08647de6
Paky2002 <pasqualecastelluccia00@gmail.com>
2025-06-02 18:28:11 +0200

    Clean Front End

--- FE/autoML/src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import DatasetUploader from '@/components/DatasetUploader/DatasetUploader.vue';
-</script>
-
-<template>
-  <main>
-    <DatasetUploader />
-  </main>
-</template>
